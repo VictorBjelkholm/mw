4bf194e1c745dd6008c2959cfd9037c1a11f2a4b
Victor Bjelkholm <victorbjelkholm@gmail.com>
2014-09-22 01:26:21 +0200

    Add token and user management

--- main.go
+++ main.go
@@ -7,13 +7,27 @@ import (
 	"fmt"
 	"github.com/codegangsta/cli"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"os/user"
 )
 
 type User struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+type Application struct {
+	Name     string `json:"name"`
+	Creator  User   `json:"_creator"`
+	Running  bool   `json:"running"`
+	DockerID string `json:"dockerId"`
+	Ip       string `json:"ip"`
+}
+
+type Token struct {
+	Value string `json:"value"`
 }
 
 func askQuestion(title string) string {
@@ -33,13 +47,70 @@ func userJsonFromParams(username string, password string) io.Reader {
 	return body
 }
 
-func saveUserLocally() bool {
-	return true
+func tokenFromJson(jsonString []byte) *Token {
+	token := &Token{}
+	err := json.Unmarshal(jsonString, &token)
+	if err != nil {
+		panic(err)
+	}
+	return token
+}
+
+func getToolFolder() string {
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+	//TODO os specific, fix me
+	path := "/home/" + user.Username + "/.modernweb"
+	return path
+}
+
+func createUserFolder() {
+	path := getToolFolder()
+	_ = os.Mkdir(path, 0777)
+}
+
+func saveTokenToDisk(token Token) error {
+	buf, _ := json.Marshal(token)
+	path := getToolFolder() + "/currentUser"
+	err := ioutil.WriteFile(path, buf, 0644)
+	return err
+}
+
+func getTokenFromDisk() string {
+	token := &Token{}
+
+	path := getToolFolder() + "/currentUser"
+
+	file, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(file, &token)
+
+	if err != nil {
+		panic(err)
+	}
+	return token.Value
+}
+
+func applicationJsonFromName(name string) io.Reader {
+	application := &Application{}
+	application.Name = name
+	buf, _ := json.Marshal(application)
+	body := bytes.NewBuffer(buf)
+	return body
 }
 
 func main() {
 
 	api := "http://192.168.33.10:3000"
+
+	createUserFolder()
 
 	app := cli.NewApp()
 	app.Name = "mw"
@@ -62,7 +133,7 @@ func main() {
 
 				body := userJsonFromParams(username, password)
 
-				_, err := http.Post(api+"/register", "application/json", body)
+				_, err := http.Post(api+"/users/register", "application/json", body)
 				if err != nil {
 					fmt.Println("Something went very wrong")
 					panic(err)
@@ -81,11 +152,16 @@ func main() {
 
 				body := userJsonFromParams(username, password)
 
-				response, err := http.Post(api+"/login", "application/json", body)
+				response, err := http.Post(api+"/users/login", "application/json", body)
 				if err != nil {
 					fmt.Println("Something went very wrong")
 					panic(err)
 				}
+				contents, err := ioutil.ReadAll(response.Body)
+
+				token := tokenFromJson(contents)
+				saveTokenToDisk(*token)
+
 				if response.StatusCode != 200 {
 					fmt.Println("Wrong username/password, please try again")
 				} else {
@@ -98,7 +174,53 @@ func main() {
 			ShortName: "i",
 			Usage:     "Initialize new application in current folder",
 			Action: func(c *cli.Context) {
-				println("completed task: ", c.Args().First())
+				if len(c.Args()) < 1 {
+					fmt.Println("You need to provide application name as first argument")
+					return
+				}
+
+				token := getTokenFromDisk()
+
+				if token == "" {
+					fmt.Println("You need to be logged in")
+					return
+				}
+
+				applicationName := c.Args()[0]
+
+				body := applicationJsonFromName(applicationName)
+
+				fmt.Println(body)
+
+				req, reqErr := http.NewRequest("POST", api+"/applications/init", body)
+				if reqErr != nil {
+					panic(reqErr)
+				}
+				req.Header.Set("Content-Type", "application/json")
+				req.Header.Set("x-token", token)
+
+				client := &http.Client{}
+				response, err := client.Do(req)
+
+				if err != nil {
+					fmt.Println("Something went very wrong")
+					panic(err)
+				}
+
+				if response.StatusCode == 201 {
+					fmt.Println("Created application '" + applicationName + "'")
+				}
+				if response.StatusCode == 401 {
+					fmt.Println("You are not logged in")
+				}
+				if response.StatusCode == 400 {
+					fmt.Println("Application '" + applicationName + "' already exists.")
+					fmt.Println("Please chose another name")
+				}
+				if response.StatusCode != 201 && response.StatusCode != 400 && response.StatusCode != 401 {
+					fmt.Println("Something went very wrong")
+					panic(response)
+				}
 			},
 		},
 	}
